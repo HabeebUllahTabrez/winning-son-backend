38d276bf0a752011f068fd151c870ca67f6cdbd4
Habeeb Ullah <56254190+HabeebUllahTabrez@users.noreply.github.com>
2025-08-27 02:07:19 +0530

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -8,11 +8,3 @@ type User struct {
 	PasswordHash string    `db:"password_hash" json:"-"`
 	CreatedAt    time.Time `db:"created_at" json:"created_at"`
 }
-
-type ProgressEntry struct {
-	ID        int       `db:"id" json:"id"`
-	UserID    int       `db:"user_id" json:"user_id"`
-	Date      time.Time `db:"date" json:"date"`
-	Amount    int       `db:"amount" json:"amount"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-}
